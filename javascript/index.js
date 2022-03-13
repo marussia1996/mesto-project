b369dd01d927085328a878a688ac1c58be69404f
marussia1996 <mariashirokova1996@gmail.com>
2022-03-13 17:34:48 +0300

    add action popup_add

--- javascript/index.js
+++ javascript/index.js
@@ -1,12 +1,21 @@
 const Profile = document.querySelector(".profile");
 const EditButton = Profile.querySelector(".profile__edit-button");
+const AddButton = Profile.querySelector(".profile__add-button");
 const ProfileName = Profile.querySelector(".profile__name");
 const ProfileJob = Profile.querySelector(".profile__job");
-const FormRedact = document.querySelector(".popup");
-const ClosePopupButton = FormRedact.querySelector(".popup__toggle");
-const FormElement = FormRedact.querySelector('.form');
+
+const PopupRedact = document.querySelector(".popup_redact");
+const FormRedact = PopupRedact.querySelector('.form_redact');
 const FormName = FormRedact.querySelector(".form__item_info_name");
 const FormJob = FormRedact.querySelector(".form__item_info_job");
+
+const PopupAdd = document.querySelector(".popup_add");
+const FormAdd = PopupAdd.querySelector('.form_add');
+const FormMesto = FormAdd.querySelector(".form__item_info_mesto");
+const FormLink = FormAdd.querySelector(".form__item_info_link");
+
+const ClosePopupRedactButton = PopupRedact.querySelector(".popup__toggle");
+const ClosePopupAddButton = PopupAdd.querySelector(".popup__toggle");
 const Elements = document.querySelector(".elements");
 const Element = Elements.querySelectorAll('.element');
 const initialCards = [
@@ -35,11 +44,10 @@ const initialCards = [
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
   }
   ];
-  for(let i = 0; i<Element.length;i++){
-    Element[i].querySelector('.element__image').src = initialCards[i].link;
-    Element[i].querySelector('.element__text').textContent = initialCards[i].name;
-  }
-
+for(let i = 0; i<Element.length;i++){
+  Element[i].querySelector('.element__image').src = initialCards[i].link;
+  Element[i].querySelector('.element__text').textContent = initialCards[i].name;
+}
 
 Elements.addEventListener('click', function(e) {
   if (e.target.classList.contains('element__like'))
@@ -49,17 +57,21 @@ Elements.addEventListener('click', function(e) {
 });
 
 // Функции открытия/закрытия попап
-function OpenPopup(){
-  FormRedact.classList.add('popup_opened');
+function OpenPopupRedact(){
+  PopupRedact.classList.add('popup_opened');
   // для полей при открытии формы редактирования информации о пользователе
   FormName.value = ProfileName.textContent;
   FormJob.value = ProfileJob.textContent;
 }
+function OpenPopupAdd(){
+  PopupAdd.classList.add('popup_opened');
+}
 function ClosePopup(){
-  FormRedact.classList.remove('popup_opened');
+  PopupRedact.classList.remove('popup_opened');
+  PopupAdd.classList.remove('popup_opened');
 }
 //Функция отправки формы
-function formSubmitHandler (evt) {
+function formSubmitHandlerRedact (evt) {
   evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
   // Получите значение полей jobInput и nameInput из свойства value
   let nameInput = FormName.value;
@@ -70,8 +82,11 @@ function formSubmitHandler (evt) {
   ClosePopup();
 }
 // События при нажатии кнопок
-EditButton.addEventListener('click', OpenPopup);
-ClosePopupButton.addEventListener('click', ClosePopup);
+EditButton.addEventListener('click', OpenPopupRedact);
+AddButton.addEventListener('click', OpenPopupAdd);
+ClosePopupRedactButton.addEventListener('click', ClosePopup);
+ClosePopupAddButton.addEventListener('click', ClosePopup);
 // Прикрепляем обработчик к форме: он будет следить за событием “submit” - «отправка»
-FormElement.addEventListener('submit', formSubmitHandler);
+FormRedact.addEventListener('submit', formSubmitHandlerRedact);
+//FormAdd.addEventListener('submit', formSubmitHandlerAdd);
 
