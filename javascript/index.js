9c3428277d421915b5fb6ebecf8adcca3a473cd4
marussia1996 <mariashirokova1996@gmail.com>
2022-03-17 20:16:01 +0300

    change adaptive/format js

--- javascript/index.js
+++ javascript/index.js
@@ -1,28 +1,30 @@
-const Profile = document.querySelector(".profile");
-const EditButton = Profile.querySelector(".profile__edit-button");
-const AddButton = Profile.querySelector(".profile__add-button");
-const ProfileName = Profile.querySelector(".profile__name");
-const ProfileJob = Profile.querySelector(".profile__job");
-
-const PopupEdit = document.querySelector(".popup_type_edit");
-const FormEdit = PopupEdit.querySelector(".form_type_edit");
-const FormName = FormEdit.querySelector(".form__item_info_name");
-const FormJob = FormEdit.querySelector(".form__item_info_job");
-
-const PopupAdd = document.querySelector(".popup_type_add");
-const FormAdd = PopupAdd.querySelector(".form_type_add");
-const FormMesto = FormAdd.querySelector(".form__item_info_mesto");
-const FormLink = FormAdd.querySelector(".form__item_info_link");
-
-const PopupImg = document.querySelector(".popup_type_image");
-const Image = PopupImg.querySelector(".popup__image");
-const Signature = PopupImg.querySelector(".popup__signature");
-
-const ClosePopupEditButton = PopupEdit.querySelector(".popup__toggle");
-const ClosePopupAddButton = PopupAdd.querySelector(".popup__toggle");
-const ClosePopupImgButton = PopupImg.querySelector(".popup__toggle");
-const Elements = document.querySelector(".elements");
-const Element = Elements.querySelectorAll(".element");
+//Переменные для работы с профилем
+const profile = document.querySelector(".profile");
+const editButton = profile.querySelector(".profile__edit-button");
+const addButton = profile.querySelector(".profile__add-button");
+const profileName = profile.querySelector(".profile__name");
+const profileJob = profile.querySelector(".profile__job");
+//Переменные для работы с формой редактирований
+const popupEdit = document.querySelector(".popup_type_edit");
+const formEdit = popupEdit.querySelector(".form_type_edit");
+const formName = formEdit.querySelector(".form__item_info_name");
+const formJob = formEdit.querySelector(".form__item_info_job");
+//Переменные для работы с формой добавления
+const popupAdd = document.querySelector(".popup_type_add");
+const formAdd = popupAdd.querySelector(".form_type_add");
+const formMesto = formAdd.querySelector(".form__item_info_mesto");
+const formLink = formAdd.querySelector(".form__item_info_link");
+//Переменные для работы с формой просмотра
+const popupImg = document.querySelector(".popup_type_image");
+const image = popupImg.querySelector(".popup__image");
+const signature = popupImg.querySelector(".popup__signature");
+//Переменные для кнопок закрытия форм
+const closePopupEditButton = popupEdit.querySelector(".popup__toggle");
+const closePopupAddButton = popupAdd.querySelector(".popup__toggle");
+const closePopupImgButton = popupImg.querySelector(".popup__toggle");
+//Переменные для работы с элементами
+const elements = document.querySelector(".elements");
+//Массив истодных элементов
 const initialCards = [
   {
     name: "Архыз",
@@ -49,87 +51,99 @@ const initialCards = [
     link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
   },
 ];
+//Добавление элемента
 function addElem(link, name) {
   const elemTemplate = document.querySelector("#elem-template").content;
   const element = elemTemplate.querySelector(".element").cloneNode(true);
   element.querySelector(".element__text").textContent = name;
   element.querySelector(".element__image").src = link;
   element.querySelector(".element__image").alt = name;
-  Elements.prepend(element);
+  elements.prepend(element);
 }
-const deleteButton = Elements.querySelectorAll(".element__delete ");
-for (let i = 0; i < Element.length; i++) {
-  Element[i].querySelector(".element__image").src = initialCards[i].link;
-  Element[i].querySelector(".element__text").textContent = initialCards[i].name;
+//Создание элемента
+function createElem(link, name) {
+  const elemTemplate = document.querySelector("#elem-template").content;
+  const element = elemTemplate.querySelector(".element").cloneNode(true);
+  element.querySelector(".element__text").textContent = name;
+  element.querySelector(".element__image").src = link;
+  element.querySelector(".element__image").alt = name;
+  elements.append(element);
 }
-Elements.addEventListener("click", function (e) {
+//Заполнение страницы 6 карточками
+for (let i = 0; i < 6; i++) {
+  createElem(initialCards[i].link, initialCards[i].name);
+}
+// Функции открытия/закрытия попап
+function openPopupEdit() {
+  popupEdit.classList.add("popup_opened");
+  // для полей при открытии формы редактирования информации о пользователе
+  formName.value = profileName.textContent;
+  formJob.value = profileJob.textContent;
+}
+function openPopupAdd() {
+  popupAdd.classList.add("popup_opened");
+}
+function openPopupImg() {
+  popupImg.classList.add("popup_opened");
+}
+function closePopupEdit() {
+  popupEdit.classList.remove("popup_opened");
+}
+function closePopupAdd() {
+  popupAdd.classList.remove("popup_opened");
+}
+function closePopupImg() {
+  popupImg.classList.remove("popup_opened");
+}
+//Функции отправки формы
+function formSubmitHandlerEdit(evt) {
+  evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+  // Получите значение полей jobInput и nameInput из свойства value
+  let nameInput = formName.value;
+  let jobInput = formJob.value;
+  // Выберите элементы, куда должны быть вставлены значения полей && Вставьте новые значения с помощью textContent
+  profileName.textContent = nameInput;
+  profileJob.textContent = jobInput;
+  closePopupEdit();
+}
+function formSubmitHandlerAdd(evt) {
+  evt.preventDefault();
+  let mestoInput = formMesto.value;
+  let linkInput = formLink.value;
+  addElem(linkInput, mestoInput);
+  closePopupAdd();
+  formMesto.value = "";
+  formLink.value = "";
+}
+// События при нажатии кнопок
+editButton.addEventListener("click", openPopupEdit);
+addButton.addEventListener("click", openPopupAdd);
+closePopupEditButton.addEventListener("click", closePopupEdit);
+closePopupAddButton.addEventListener("click", closePopupAdd);
+closePopupImgButton.addEventListener("click", closePopupImg);
+// Прикрепляем обработчик к форме: он будет следить за событием “submit” - «отправка»
+formEdit.addEventListener("submit", formSubmitHandlerEdit);
+formAdd.addEventListener("submit", formSubmitHandlerAdd);
+//Функции обработки событий для элементов
+elements.addEventListener("click", function (e) {
   if (e.target.classList.contains("element__like")) {
     // если клик по like
     e.target.classList.toggle("element__like_active");
   } // если есть модификатор _active уберем его, иначе добавим
 });
-Elements.addEventListener("click", function (e) {
+elements.addEventListener("click", function (e) {
   if (e.target.classList.contains("element__delete")) {
     e.target.parentNode.remove();
   }
 });
-Elements.addEventListener("click", function (e) {
+elements.addEventListener("click", function (e) {
   if (e.target.classList.contains("element__image")) {
-    OpenPopupImg();
-    Image.src = e.target.src;
+    openPopupImg();
+    image.src = e.target.src;
     let parentElem = e.target.parentNode;
     let text = parentElem
       .querySelector(".element__group")
       .querySelector(".element__text");
-    Signature.textContent = text.textContent;
+    signature.textContent = text.textContent;
   }
 });
-// Функции открытия/закрытия попап
-function OpenPopupEdit() {
-  PopupEdit.classList.add("popup_opened");
-  // для полей при открытии формы редактирования информации о пользователе
-  FormName.value = ProfileName.textContent;
-  FormJob.value = ProfileJob.textContent;
-}
-function OpenPopupAdd() {
-  PopupAdd.classList.add("popup_opened");
-}
-function OpenPopupImg() {
-  PopupImg.classList.add("popup_opened");
-}
-function ClosePopup() {
-  PopupEdit.classList.remove("popup_opened");
-  PopupAdd.classList.remove("popup_opened");
-  PopupImg.classList.remove("popup_opened");
-}
-
-//Функция отправки формы
-function formSubmitHandlerEdit(evt) {
-  evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-  // Получите значение полей jobInput и nameInput из свойства value
-  let nameInput = FormName.value;
-  let jobInput = FormJob.value;
-  // Выберите элементы, куда должны быть вставлены значения полей && Вставьте новые значения с помощью textContent
-  ProfileName.textContent = nameInput;
-  ProfileJob.textContent = jobInput;
-  ClosePopup();
-}
-
-function formSubmitHandlerAdd(evt) {
-  evt.preventDefault();
-  let mestoInput = FormMesto.value;
-  let linkInput = FormLink.value;
-  addElem(linkInput, mestoInput);
-  ClosePopup();
-  FormMesto.value = "";
-  FormLink.value = "";
-}
-// События при нажатии кнопок
-EditButton.addEventListener("click", OpenPopupEdit);
-AddButton.addEventListener("click", OpenPopupAdd);
-ClosePopupEditButton.addEventListener("click", ClosePopup);
-ClosePopupAddButton.addEventListener("click", ClosePopup);
-ClosePopupImgButton.addEventListener("click", ClosePopup);
-// Прикрепляем обработчик к форме: он будет следить за событием “submit” - «отправка»
-FormEdit.addEventListener("submit", formSubmitHandlerEdit);
-FormAdd.addEventListener("submit", formSubmitHandlerAdd);
