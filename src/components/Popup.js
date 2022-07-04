326d370788b8c3e63194255229c72bdc41d9b43f
Daria Gusar <pretzel1104@yandex.ru>
2022-07-04 20:34:10 +0300

    оптимизация Popup

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,18 +1,17 @@
 export default class Popup {
   constructor(selectorPopup) {
     this._selectorPopup = selectorPopup;
+    this._popup = document.querySelector(this._selectorPopup);
   }
   // Функции открытия/закрытия попап
   openPopup() {
-    document.querySelector(this._selectorPopup).classList.add("popup_opened");
+    this._popup.classList.add("popup_opened");
     document.addEventListener("keydown", (evt) => {
       this._closePopupByEsc(evt);
     });
   }
   closePopup() {
-    document
-      .querySelector(this._selectorPopup)
-      .classList.remove("popup_opened");
+    this._popup.classList.remove("popup_opened");
     document.removeEventListener("keydown", (evt) => {
       this._closePopupByEsc(evt);
     });
@@ -24,15 +23,12 @@ export default class Popup {
     }
   }
   setEventListeners() {
-    document
-      .querySelector(this._selectorPopup)
-      .addEventListener("click", (evt) => {
-        if (evt.target.classList.contains("popup")) {
-          this.closePopup();
-        }
-      });
-    document
-      .querySelector(this._selectorPopup)
+    this._popup.addEventListener("click", (evt) => {
+      if (evt.target.classList.contains("popup")) {
+        this.closePopup();
+      }
+    });
+    this._popup
       .querySelector(".popup__toggle")
       .addEventListener("click", () => {
         this.closePopup();
